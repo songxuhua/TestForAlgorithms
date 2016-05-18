c58a4ff45731dabbe3bb5e49ff8d3694376f6f27
宋旭华 <songxuhua@ddkl.cn>
2016-05-18 09:56:05 +0800

    Initial Commit

---
+++ TestForAlgorithms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestForAlgorithms
+//
+//  Created by 宋华 on 16/5/18.
+//  Copyright © 2016年 宋华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
